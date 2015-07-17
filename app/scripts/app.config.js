86482fd7e519f21c9afb1958903d6c7498270d67
Krishna Nandula <knandula@factset.com>
2015-07-17 18:59:08 +0530

    google new clientid

--- app/scripts/app.config.js
+++ app/scripts/app.config.js
@@ -4,7 +4,7 @@
  * Created by knandula on 7/9/2015.
  */
 angular.module('fictiontree2App').config(function($urlRouterProvider,$stateProvider,$authProvider,API_URL){
-  var clientId = '627609735136-csn9l71i3plm238nhm4qticft60smd81.apps.googleusercontent.com';
+  var clientId = '627609735136-25od75c7mf6rijeqlps65288uir7undm.apps.googleusercontent.com';
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
@@ -44,7 +44,7 @@ angular.module('fictiontree2App').config(function($urlRouterProvider,$stateProvi
 
 
     $authProvider.google({
-      clientId:'627609735136-csn9l71i3plm238nhm4qticft60smd81.apps.googleusercontent.com',
+      clientId:'627609735136-25od75c7mf6rijeqlps65288uir7undm.apps.googleusercontent.com',
       url: API_URL + 'auth/google'
     })
 
