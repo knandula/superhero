99370bbac3040e83856e1131889a7e8b0773c6a6
Krishna Nandula <knandula@factset.com>
2015-07-29 03:16:01 +0530

    cors

--- app/scripts/services/userdataservice.js
+++ app/scripts/services/userdataservice.js
@@ -6,6 +6,7 @@ angular.module('fictiontree2App').factory('userDataService', function ($http,$q,
   var coverurl = API_URL + 'getcoverpicdata';
   var profileurl = API_URL + 'getprofilepicdata';
 
+
     return {
       getCoverPic : getCover,
       getProfilePic : getProfile
@@ -14,9 +15,16 @@ angular.module('fictiontree2App').factory('userDataService', function ($http,$q,
 
 
     function getCover() {
+      var headers = {
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      };
       return $http({
          method:'POST',
          url: coverurl,
+        headers:headers,
         data:userService.userdata
       }).then(sendResponseData)
         .catch(sendError);
@@ -24,9 +32,16 @@ angular.module('fictiontree2App').factory('userDataService', function ($http,$q,
 
 
     function getProfile() {
+      var headers = {
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      };
       return $http({
         method:'POST',
         url: profileurl,
+        headers:headers,
         data:userService.userdata
       }).then(sendResponseDataProfile)
         .catch(sendError);
