45b58c14b78f270d51abfaa1fe47da3ab694c6e8
Krishna Nandula <knandula@factset.com>
2015-07-29 12:59:11 +0530

    auto

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-angular.module('fictiontree2App').controller('MainCtrl', function ($scope,$state,auth,authToken,alert,$auth) {
+angular.module('fictiontree2App').controller('MainCtrl', function ($scope,$state,userService,auth,authToken,alert,$auth) {
 
     $scope.authenticate = function(provider){
         $auth.authenticate(provider).then(function(res){
           console.log(res);
-          alert('success','Welcome  ' + res.data.user.displayName + " !");
+          userService.userdata = res.data.user;
           $state.go('account');
         },function(err){
           alert('warning','Something went wrong :(   ',err.message);
