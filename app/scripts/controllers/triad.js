73047f228a93cdab9848b04b30747ba2ed6d8327
Krishna Nandula <knandula@factset.com>
2015-08-05 15:29:58 +0530

    testing codeship

--- app/scripts/controllers/triad.js
+++ app/scripts/controllers/triad.js
@@ -7,22 +7,7 @@ angular.module('fictiontree2App').controller('TriadCtrl', function (Service,$sco
     switch(args.from)
     {
       case 'searchcharacter':
-        // check if the character name is existing or new ?
-        // if existing then get the associated movies and actors in the movies
-        //
         $scope.charinput = args.message;
-
-
-
-
-
-
-
-
-
-
-
-
             break;
       case 'searchMovie':
             break;
